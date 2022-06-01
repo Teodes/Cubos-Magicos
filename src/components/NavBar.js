5f64f6a01619e13a178ee73de8f48c70fe34e0b4
Teodes <jgrachot@gmail.com>
2022-05-31 21:33:09 -0300

    NavBar Component Added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,32 @@
 import logo from "../images/logo.png";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
   return (
-    <nav>
-      <div className="Logo">
-        <img src={logo} alt="Logo" />
-        <span>Magicubes</span>
-      </div>
-      <ul>
-        <li>
-          <a href="">Cúbicos</a>
-        </li>
-        <li>
-          <a href="">Cuboides</a>
-        </li>
-        <li>
-          <a href="">Minx</a>
-        </li>
-        <li>
-          <a href="">Engranajes</a>
-        </li>
-      </ul>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo" />
+          <span>Magicubes</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#cubicos">Cúbicos</Nav.Link>
+            <Nav.Link href="#cuboides">Cuboides</Nav.Link>
+            <Nav.Link href="#minx">Minx</Nav.Link>
+            <Nav.Link href="#engranajes">Engranajes</Nav.Link>
+            <Nav.Link href="#carrito">
+              <FontAwesomeIcon icon={faShoppingCart} />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 export default NavBar;
