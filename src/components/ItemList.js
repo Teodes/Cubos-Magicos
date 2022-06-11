cbca79e418a599bd13ccbf4e9a9c249e280f8740
Teodes <jgrachot@gmail.com>
2022-06-10 23:38:30 -0300

    Loading Text Implemented

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,22 +4,31 @@ import Item from "./Item";
 
 export default function ItemList() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getFetch(products).then((response) => {
-      setProducts(response);
-    });
+    getFetch(products)
+      .then((response) => {
+        setProducts(response);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   });
   return (
     <>
-      {products.map((product) => (
-        <Item
-          name={product.name}
-          key={product.id}
-          stock={product.stock}
-          src={product.url}
-        />
-      ))}
+      {loading ? (
+        <h1>Cargando...</h1>
+      ) : (
+        products.map((product) => (
+          <Item
+            name={product.name}
+            key={product.id}
+            stock={product.stock}
+            src={product.url}
+          />
+        ))
+      )}
     </>
   );
 }
