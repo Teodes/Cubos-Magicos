0ed39c15214f1e14a24371cc8ce9b902318df62e
Teodes <jgrachot@gmail.com>
2022-06-20 21:09:56 -0300

    Message Fixed

--- src/Helpers/addToCart.js
+++ src/Helpers/addToCart.js
@@ -1,14 +1,17 @@
 import { toast } from "react-toastify";
 
 export function addToCart(qty) {
-  toast.success(`${qty} ítems añadidos`, {
-    position: "bottom-right",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "dark",
-  });
+  toast.success(
+    `${qty} ${qty === 1 ? "producto" : "productos"} agregados al carrito`,
+    {
+      position: "bottom-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    }
+  );
 }
