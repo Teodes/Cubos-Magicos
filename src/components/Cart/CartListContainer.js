75dcf10d49cab7e702e297793ba4e5faa88dbb2e
Teodes <jgrachot@gmail.com>
2022-07-06 00:04:38 -0300

    Orders implemented

--- src/components/Cart/CartListContainer.js
+++ src/components/Cart/CartListContainer.js
@@ -1,23 +1,36 @@
 import { Button, Container } from "react-bootstrap";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CartContext } from "../../context/cartContext";
 import CartItem from "./CartItem";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 export default function CartListContainer() {
   const { clearCart, cart } = useContext(CartContext);
+  const [purchaseId, setPurchaseId] = useState(false);
 
   function CartButtons() {
     if (cart.length === 0) {
-      return (
-        <div>
-          <h1>El carrito se encuentra vacío</h1>
-          <Link to={"/"}>
-            <Button>Ir a la Tienda</Button>
-          </Link>
-        </div>
-      );
+      if (purchaseId) {
+        return (
+          <div>
+            <h1>Compra exitosa!</h1>
+            <h3>
+              Tu orden de compra es: <strong>{purchaseId}</strong>
+            </h3>
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <h1>El carrito se encuentra vacío</h1>
+            <Link to={"/"}>
+              <Button>Ir a la Tienda</Button>
+            </Link>
+          </div>
+        );
+      }
     } else {
       return (
         <>
@@ -39,7 +52,8 @@ export default function CartListContainer() {
             <Button
               className="m-1"
               variant="success"
-              onClick={() => {
+              onClick={(e) => {
+                generateOrder(e);
                 clearCart();
                 toast.success("¡Compra Finalizada!", {
                   position: "bottom-right",
@@ -65,6 +79,37 @@ export default function CartListContainer() {
   cart.forEach((item) => {
     totalPrice = totalPrice + item.qty * item.price;
   });
+
+  function generateOrder(e) {
+    e.preventDefault();
+
+    let order = {};
+    order.buyer = {
+      name: "Joaco",
+      email: "jgrachor@gmail.com",
+      tel: "3512345678",
+    };
+    order.total = totalPrice;
+
+    order.date = new Date().toLocaleString("es-AR");
+
+    order.items = cart.map((item) => {
+      const id = item.id;
+      const name = item.name;
+      const price = item.price * item.qty;
+
+      return { id, name, price };
+    });
+
+    console.log(order);
+
+    const db = getFirestore();
+    const orderCollection = collection(db, "orders");
+    addDoc(orderCollection, order).then((resp) => {
+      setPurchaseId(resp.id);
+    });
+    //updateDoc para actualizar valores.
+  }
 
   return (
     <div className="py-5">
