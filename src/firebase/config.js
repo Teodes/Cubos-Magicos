344f0d94c5bbc32e69f82b15315fc11733e01174
Teodes <jgrachot@gmail.com>
2022-07-13 23:15:32 -0300

    TODO Added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 
+//TODO: Reemplazar los Lorem Ipsum en Firestore
+
 const firebaseConfig = {
   apiKey: "AIzaSyAFelV4BG-4QcdwVrHGQs25j60YHw0rI_k",
   authDomain: "cubos-magicos.firebaseapp.com",
